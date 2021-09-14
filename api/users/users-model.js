60f0fdfa6e49cfed272a03c3e797c180212251cf
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-09-14 15:10:25 -0500

    Add findById to users model

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -14,16 +14,10 @@ function findBy(filter) {
 }
 
 function findById(user_id) {
-  /**
-    You will need to join two tables.
-    Resolves to the user with the given user_id.
-
-    {
-      "user_id": 2,
-      "username": "sue",
-      "role_name": "instructor"
-    }
-   */
+  return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'password', 'role_name')
+    .where('users.user_id', user_id).first()
 }
 
 /**
