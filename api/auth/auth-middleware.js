3ce30bf4668f3e5d725a900948143ee0ded6b847
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-09-14 16:38:29 -0500

    Add restricted middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,23 +1,20 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 const { findBy } = require('../users/users-model')
+const jwt = require('jsonwebtoken')
 
 const restricted = (req, res, next) => {
-  /*
-    If the user does not provide a token in the Authorization header:
-    status 401
-    {
-      "message": "Token required"
+  const token = req.headers.authorization
+  if (!token) {
+    return next({ status: 401, message: 'Token required' })
+  } 
+  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+    if (err) {
+      next({ status: 401, message: 'Token invalid' })
+    } else {
+      req.decodedToken = decodedToken
+      next()
     }
-
-    If the provided token does not verify:
-    status 401
-    {
-      "message": "Token invalid"
-    }
-
-    Put the decoded token in the req object, to make life easier for middlewares downstream!
-  */
-  next()
+  })
 }
 
 const only = role_name => (req, res, next) => {
